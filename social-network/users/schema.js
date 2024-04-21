a1edf1282d3015528fac80b322e81a233a6bdfc1
jknopp12 <129903306+jknopp12@users.noreply.github.com>
2024-04-20 22:00:21 -0400

    update

--- social-network/users/schema.js
+++ social-network/users/schema.js
@@ -3,7 +3,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstName: String,
-    email: String,
+    emairsl: String,
     lastName: String,
     role: {
       type: String,
